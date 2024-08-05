78a51896a3689f8ff08fa576e9488d59829b24e5
Ayberk <ayberkcetinkaya54@gmail.com>
2024-08-05 22:16:16 +0300

    050724

---
+++ src/pages/pages_1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function pages_1() {
+  return (
+    <Background>
+      <Baslik>Teknolojik Yemekler</Baslik>
+      <Aciklama>KOD ACIKTIRIR PİZZA, DOYURUR</Aciklama>
+      <Buton>ACIKTIM</Buton>
+    </Background>
+  );
+}
+
+export default pages_1;
